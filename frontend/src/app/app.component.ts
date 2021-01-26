ab09b0c67b80daf29756f8fa1f8967976bcb40d8
raulfranck <raulengfranck@gmail.com>
2021-01-26 17:22:22 -0300

    httpClient

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,18 +9,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  photos = [
-    {
-      url: 'https://images.pexels.com/photos/3952549/pexels-photo-3952549.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-      description: 'Montanhas'
-    },
-    {
-      url: 'https://images.pexels.com/photos/6159755/pexels-photo-6159755.jpeg?cs=srgb&dl=pexels-eva-elijas-6159755.jpg&fm=jpg',
-      description: 'Rochas'
-    },
-    {
-      url: 'https://images.pexels.com/photos/5288214/pexels-photo-5288214.jpeg?cs=srgb&dl=pexels-mohamed-abdelsadig-5288214.jpg&fm=jpg',
-      description: 'Lua'
-    }
-  ];
+  photos = []
+  
+  constructor(http: HttpClient) {
+    console.log(http)
+  }
 }
